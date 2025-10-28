612c4eab6b4893846bda9190f1818bf0d8319986
Arthur Saldanha <saldanhadev@gmail.com>
2025-10-28 12:50:13 -0300

    feat: add env in authClient

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { adminClient } from "better-auth/client/plugins";
 import { stripeClient } from "@better-auth/stripe/client";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL,
   plugins: [
     adminClient(),
     stripeClient({
